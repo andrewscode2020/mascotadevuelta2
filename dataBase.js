27c83331913a2754bdfd1d0f19d1ea284650cbea
andrewscode2020 <greatandres@gmail.com>
2020-11-13 21:30:11 -0500

    First commit for connecting to Heroku

--- dataBase.js
+++ dataBase.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
+const {MONGODB} = require('./config')
 
 // DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
 mongoose.set('useCreateIndex', true);
 
-mongoose.connect('mongodb://127.0.0.1:27017/mascotaDeVuelta', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
