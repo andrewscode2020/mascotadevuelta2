2fe699c8dd2211e2784e1ce6849657f8d9e001a8
andrewscode2020 <greatandres@gmail.com>
2020-11-17 08:51:24 -0500

    Some minor changes to dataBase.js file

--- dataBase.js
+++ dataBase.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose')
 const {MONGODB} = require('./config')
 
-// DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
-mongoose.set('useCreateIndex', true);
-
-mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
+db.on('error', console.error.bind(console, 'Error de conección:'))
 db.once('open', function () {
-  console.log('Conexión exitosa con la base de datos')
+  console.log('Conexión exitosa con la base de datos MongoDB.')
 })
