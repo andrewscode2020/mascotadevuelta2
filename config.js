d01fa82c7900e760e0300bcb1e9c52ea0c2f1e61
andrewscode2020 <greatandres@gmail.com>
2020-11-19 10:53:01 -0500

    Changed config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-// module.exports = {
-//     PORT: process.env.PORT || 3000,
-//     MONGODB: process.env.MONGODB || 'mongodb://127.0.0.1:27017/mascotaDeVuelta2'
-// }
-
 module.exports = {
-    PORT: 3000,
-    MONGODB: 'mongodb://127.0.0.1:27017/mascotaDeVuelta2'
+    PORT: process.env.PORT || 3000,
+    MONGODB: process.env.MONGODB || 'mongodb://127.0.0.1:27017/mascotaDeVuelta2'
 }
+
+// module.exports = {
+//     PORT: 3000,
+//     MONGODB: 'mongodb://127.0.0.1:27017/mascotaDeVuelta2'
+// }
